7ae25f031d12aca0836c98b46f52c32a00093a18
NikitaSysoev <na.sysoev@gmail.com>
2018-10-31 21:59:29 +0300

    added api method hello, installing prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  parser: 'babel-eslint',
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    plugins: ['prettier'],
+  },
+};
---
+++ alfa/src/api/api.js
@@ -0,0 +1,9 @@
+class api {  
+  static hello() {
+    return fetch(`http://localhost:8080/hello`)
+      .then(res => res.json())
+      .then(data => data.message);
+  }
+}
+
+export default api;

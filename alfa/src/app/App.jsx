a98329d803544045962f7e85c6cda1ea724c0826
NikitaSysoev <na.sysoev@gmail.com>
2018-11-03 15:34:17 +0300

    testing eslint v2

--- alfa/src/app/App.jsx
+++ alfa/src/app/App.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import api from '../api';
 
 export default class App extends Component {
-  state = {
-    message: null,
-  };
+    state = {
+      message: null,
+    };
 
-  componentDidMount() {
-    api.hello().then(message => this.setState({ message }));
-  }
+    componentDidMount() {
+      api.hello().then(message => this.setState({ message }));
+    }
 
-  render() {
-    const { message } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{message}</p>
-        </header>
-      </div>
-    );
-  }
+    render() {
+      const { message } = this.state;
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>{message}</p>
+          </header>
+        </div>
+      );
+    }
 }

6cc1e24a935ea696564ab76f81795d504729f794
NikitaSysoev <na.sysoev@gmail.com>
2018-11-03 14:17:38 +0300

    testing eslint

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'alfa', 'build')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
 });
 
 app.get('/hello', (req, res) => {
-  res.status(200).send({
-    message: 'Hello world',
-  });
+	res.status(200).send({
+		message: 'Hello world',
+	});
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'alfa', 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'alfa', 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening`));
+app.listen(process.env.PORT || 8080, () => console.log('Listening'));

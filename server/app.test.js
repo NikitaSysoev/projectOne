a132203c717f940847f33fe464e7ee258352e410
NikitaSysoev <na.sysoev@gmail.com>
2018-11-14 20:06:23 +0300

    app.test.js is refactored. Added async function

--- server/app.test.js
+++ server/app.test.js
@@ -2,10 +2,8 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Test the server/app', () => {
-  test('It should return Hello world', (done) => {
-    request(app).get('/hello').end((error, response) => {
-      expect(response.body.message).toBe('Hello world');
-      done();
-    });
+  test('It should return Hello world', async () => {
+    const response = await request(app).get('/hello');
+    expect(response.body.message).toBe('Hello world');
   });
 });
